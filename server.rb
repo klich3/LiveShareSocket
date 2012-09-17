07a3f94e331764b7ff107ef16324c15ba3661f04
Cleiton Souza <cleitaum1@gmail.com>
2012-09-17 00:54:26 -0300

    Testes de envio de arquivo na classe server

--- server.rb
+++ server.rb
@@ -1,28 +1,3 @@
-
-
-def dataReceived(session, data)
-        a = data.split(':')
-        puts a
-        if (len(a) > 1)
-            command = a[0]
-            content = a[1]
- 
-            msg = ""
-            if (command == "iam")
-                session.name = content
-                msg = session.name + " has joined"
- 
-            elif (command == "msg")
-                msg = session.name + ": " + content
-                puts msg
-            end
-         end
- 
-            for c in session.factory.clients do
-                c.message(msg)
-            end
-end
-
 #!/usr/bin/ruby
 require 'socket'
 puts "Starting up server..."
@@ -33,16 +8,25 @@ server = TCPServer.new(2008)
 
 # setup to listen and accept connections
 while connection = server.accept
-  while line = connection.gets
-    break if line =~ /quit/
-  #  puts line
-    connection.puts "Received!"
-    
-    commandArray = line.split(":")
-	
-	puts commandArray.length;
-    
-  end
+
+  connection.puts "---Hi, you are connected---"	
+  
+  	  data = ""
+  	  i=0
+	  while content = connection.gets
+		break if content =~ /quit/
+	  #  puts line
+		connection.puts "Received!"
+		#puts content
+		data += content
+		puts "passei #{i}"
+		i +=1
+		if (i > 591)
+			break
+		end
+	  end
+	  	puts "vou criar o arquivo"
+	  	File.open("out.mp4", "w+") { |file| file.write(data) }
 
   connection.puts "Closing the connection. Bye!"
   connection.close
