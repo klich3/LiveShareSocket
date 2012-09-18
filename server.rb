2824c935be9b65551ff07167aa977cd3b0f28802
cleitaum <cleitaum1@gmail.com>
2012-09-18 18:48:43 -0300

    recebendo o arquivo

--- server.rb
+++ server.rb
@@ -1,64 +1,37 @@
 #!/usr/bin/ruby
 require 'socket'
-#puts "Starting up server..."
+
+puts "Starting up server..."
 # establish the server
 
 ## Server established to listen for connections on port 2008
-
-SIZE = 1024 * 1024 * 10
-
 server = TCPServer.new(2008)
-puts "Server listening..." 
-client = server.accept       
 
-while content = client.gets
-
-	break if content =~ /quit/
-
-    	puts "Gets ?"
-
-  File.open('output.mp4', 'w+') do |file|
-    while chunk = client.read(SIZE)
-
-      file.write(chunk)
-    end
-end
-  
-  connection.puts "Closing the connection. Bye!"
-  connection.close
-end
-
-
-#!/usr/bin/ruby
-#require 'socket'
-#puts "Starting up server..."
-# establish the server
-
-## Server established to listen for connections on port 2008
-#server = TCPServer.new(2008)
+SIZE = 512
 
 # setup to listen and accept connections
 #while connection = server.accept
-
-#  connection.puts "---Hi, you are connected---"	
+connection = server.accept
   
-  #	  data = ""
- # 	  i=0
-#	  while content = connection.gets
-#		break if content =~ /quit/
-	  #  puts line
-#		connection.puts "Received!"
-		#puts content
-#		data += content
-#		puts "passei #{i}"
-#		i +=1
-#		if (i > 591)
-#			break
-#		end
-#	  end
-#	  	puts "vou criar o arquivo"
-#	  	File.open("out.mp4", "w+") { |file| file.write(data) }
+  while content = connection.gets
+		break if content =~ /quit/
 
- # connection.puts "Closing the connection. Bye!"
-#  connection.close
+		connection.puts "Received!"
+		
+		i = 0
+
+		File.open('output.mp4', 'w+') do |file|
+			
+			while chunk = connection.read(SIZE)
+				puts "passei #{i}"
+				i+=1
+				file.write(chunk)
+			end
+		
+			puts "finished"
+		end		
+  end
+	
+  connection.puts "Closing the connection. Bye!"
+  connection.close
 #end
